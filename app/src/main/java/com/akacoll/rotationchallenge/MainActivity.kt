94f14ccc5960f2e065d668c34bafceb133219775
Akacoll <gabriel.akacoll@gmail.com>
2022-09-01 17:09:09 -0300

    Hotfix

--- app/src/main/java/com/akacoll/rotationchallenge/MainActivity.kt
+++ app/src/main/java/com/akacoll/rotationchallenge/MainActivity.kt
@@ -6,28 +6,32 @@ import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
 
+
 class MainActivity : AppCompatActivity() {
-    private var counter = 0
+    companion object {
+        const val KEY_COUNTER = "KEY_COUNTER"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text = savedInstanceState?.getString("KEY_TEXT") ?: ""
         val tvMain = findViewById<TextView>(R.id.tv_main)
         val btnCounter = findViewById<Button>(R.id.btn_add)
         btnCounter.setOnClickListener {
+            var counter = tvMain.text.toString().toInt()
             counter++
             tvMain.text = counter.toString()
         }
+        val text = savedInstanceState?.getString(KEY_COUNTER) ?: "0"
         tvMain.text = text
 
     }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
 
         val textView = findViewById<TextView>(R.id.tv_main)
         val text = textView.text.toString()
-        outState.putString("KEY_TEXT", text)
+        outState.putString(KEY_COUNTER, text)
     }
 }
